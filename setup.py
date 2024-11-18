fbf157eb0e68db61f838552bc31949feff2645cc
Vincent Omondi <larntechconsultants@gmail.com>
2024-11-18 08:36:09 +0300

    setup configigurations

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='autochektools',
+    version='0.0.1',
+    description='Autocheck data engineering private utilities package.',
+    url='git@github.com:Autochek-Africa/autochek-de-tools.git',
+)
